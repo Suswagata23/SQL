325832af5f48bada105603cb4f30e0ca739e2826
Suswagata Chakraborty <38370596+Suswagata23@users.noreply.github.com>
2025-03-21 00:50:46 +0530

    Create Weather_observation_station_8.sql

---
+++ HackerRank/Weather_observation_station_8.sql
@@ -0,0 +1,28 @@
+--Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+
+select distinct city from station
+where city like ('A%a')
+or city like ('A%e')
+or city like ('A%i')
+or city like ('A%o')
+or city like ('A%u')
+or city like ('E%a')
+or city like ('E%e')
+or city like ('E%i')
+or city like ('E%o')
+or city like ('E%u')
+or city like ('I%a')
+or city like ('I%e')
+or city like ('I%i')
+or city like ('I%o')
+or city like ('I%u')
+or city like ('O%a')
+or city like ('O%e')
+or city like ('O%i')
+or city like ('O%o')
+or city like ('O%u')
+or city like ('U%a')
+or city like ('U%e')
+or city like ('U%i')
+or city like ('U%o')
+or city like ('U%u');
